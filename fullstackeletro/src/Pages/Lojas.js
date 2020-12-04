498ca0bb6021eb42c7532f407b53792e754458c8
Lucas Xavier <lucasxavierclaindeabreu@gmail.com>
2020-12-04 19:00:12 -0300

    alteração de endereço

--- fullstackeletro/src/Pages/Lojas.js
+++ fullstackeletro/src/Pages/Lojas.js
@@ -1,16 +1,14 @@
 import { Container } from 'react-bootstrap'
 export default function Lojas() {
     return (
-        <Container fluid className='d-flex '>
-            <div>
-                <h2>Rua são carmo</h2>
+        <Container fluid className='d-flex jumbotron jumbotron-fluid'>
+         
+            <div className='mx-auto text-center'>
+                <h2>Rua guilherme veloso</h2>
+                <p>Praça seca - Rio de janeiro -RJ</p>
+                <p>Jacarepaguá</p>
             </div>
-            <div>
-                <h2>Rua são carmo</h2>
-            </div>
-            <div>
-                <h2>Rua são carmo</h2>
-            </div>
+           
         </Container>
     )
 }
