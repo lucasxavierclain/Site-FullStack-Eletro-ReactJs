a3f24801160d767eca2590d587cb25a11312f037
lucasxavierclain <61299094+lucasxavierclain@users.noreply.github.com>
2020-12-04 21:12:39 -0300

    Update script-fullstack.sql

--- bd/script-fullstack.sql
+++ bd/script-fullstack.sql
@@ -1,4 +1,4 @@
-create database fullstack;
+CREATE SCHEMA `fullstack` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 use fullstack;
 
 CREATE TABLE `fullstack`.`produtos` (
@@ -9,7 +9,10 @@ CREATE TABLE `fullstack`.`produtos` (
   `preco` DECIMAL(8,2) NULL,
   `precoAnterior` DECIMAL(8,2) NULL,
   `nomeImagem` VARCHAR(70) NULL,
-  PRIMARY KEY (`idprodutos`));
+  PRIMARY KEY (`idprodutos`))
+  ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
 CREATE TABLE `fullstack`.`usuario` (
@@ -17,7 +20,10 @@ CREATE TABLE `fullstack`.`usuario` (
   `nome` VARCHAR(60) NULL,
   `email` VARCHAR(50) NULL,
   `telefone` DECIMAL(11) NULL,
-  PRIMARY KEY (`idusuario`));
+  PRIMARY KEY (`idusuario`))ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
 CREATE TABLE `fullstack`.`pedido` (
   `idpedido` INT NOT NULL AUTO_INCREMENT,
   `idusuario` INT NULL,
@@ -26,7 +32,10 @@ CREATE TABLE `fullstack`.`pedido` (
   `nome` VARCHAR(60) NULL,
   `email` VARCHAR(50) NULL,
   `telefone` DECIMAL(11) NULL,
-  PRIMARY KEY (`idpedido`));
+  PRIMARY KEY (`idpedido`))
+  ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
   
 ALTER TABLE `fullstack`.`pedido` 
 ADD INDEX `idprodutos_idx` (`idprodutos` ASC) VISIBLE;
@@ -38,6 +47,7 @@ ADD CONSTRAINT `idprodutos`
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
+
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('celular', 'Celular','Motorola V8', '1200','2000', '/imagens/celular-motorola.jpg');
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('celular', 'Celular','Samsung A23', '800', '5000','/imagens/celular-samsung.jpg');
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('celular', 'Celular','Iphone 11', '1100', '1500','/imagens/celular-iphone.jpg');
@@ -47,13 +57,6 @@ INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`,
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('televisao', 'TV','LG', '1500', '4000','/imagens/lg-tv.jpg');
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('televisao', 'TV','Philco', '1200','2000', '/imagens/philco-tv.jpg');
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('televisao', 'TV','Samsung', '2000','3500', '/imagens/samsung-tv.jpg');
-INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('maquinaDeLavar', 'Máquina de lavar','Consul', '1200','2500', '/imagens/maquina-de-lavar-consul.jpg');
+INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('maquinaDeLavar', 'Maquina de lavar','Consul', '1200','2500', '/imagens/maquina-de-lavar-consul.jpg');
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('microondas', 'Microondas','Electrolux', '800', '2000','/imagens/microondas-electrolux.jpg');
 INSERT INTO `fullstack`.`produtos` (`categoria`, `produto`,`descricao`, `preco`, `precoAnterior`,`nomeImagem`) VALUES ('microondas', 'Microondas','Ret', '900', '2000','/imagens/microondas-ret.png');
-
-
-
--- exemplo de select 
-SELECT * FROM pedido 
-JOIN produtos ON pedido.idprodutos = produtos.idprodutos
-;
